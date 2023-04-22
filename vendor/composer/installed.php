f4c3e6769f820311df59bbf3bc8add4f0fca0585
Nii Armar Adjaidoo <braniiblack@gmail.com>
2023-04-22 03:13:39 +0000

    first commit, step one

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bb8ff8c085228f0866f3799a4001e1076cd8a49c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bb8ff8c085228f0866f3799a4001e1076cd8a49c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nowakowskir/php-jwt' => array(
+            'pretty_version' => '2.1.1',
+            'version' => '2.1.1.0',
+            'reference' => 'df6f9961d8c365f38742d2d6352254d6b8ae9abd',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../nowakowskir/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
